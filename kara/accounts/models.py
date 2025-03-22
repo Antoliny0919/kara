b3f042de0e3429ddba4e3dfb9f0f4481063c239c
antoliny0919 <antoliny0919@gmail.com>
2025-03-23 08:33:58 +0900

    Updated -- User모델과 1:1 연결된 UserProfile모델 추가.

--- kara/accounts/models.py
+++ kara/accounts/models.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from .fields import DefaultOneToOneField
 
 
 class User(AbstractUser):
     first_name = None
     last_name = None
+
+
+class UserProfile(models.Model):
+    user = DefaultOneToOneField(User, create=True, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    bio_image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
