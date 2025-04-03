94ea188e1ea6b3b6fa14d1b63a999fd35ad51e78
Antoliny Lee <antoliny0919@gmail.com>
2025-04-04 07:40:12 +0900

    Updated -- 이메일 인증로직 구현. (#34)

--- kara/accounts/tests/test_views.py
+++ kara/accounts/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.core import mail
 from django.test import Client, TestCase
 from django.urls import reverse
 
@@ -26,4 +27,55 @@ class SignupViewTests(TestCase):
             follow=True,
         )
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Please check your inbox!")
         self.assertTrue(User.objects.filter(username="strawberry").exists())
+        self.assertFalse(
+            User.objects.get(username="strawberry").profile.email_confirmed
+        )
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, "Kara Email Confirmation")
+        self.assertIn(
+            "Please enter the 6-digit email verification code on the "
+            "email verification page.",
+            mail.outbox[0].body,
+        )
+        self.assertIn("pending_email_confirmation", self.client.session)
+        pending_email_confirmation = self.client.session["pending_email_confirmation"]
+        self.assertEqual(pending_email_confirmation["username"], "strawberry")
+        self.assertEqual(pending_email_confirmation["email"], "strawberry@farm.com")
+
+
+class ConfirmEmailVerificationCodeViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.client.post(
+            reverse("signup"),
+            data={
+                "username": "watermelon",
+                "email": "watermelon@farm.com",
+                "password1": "*password*",
+                "password2": "*password*",
+            },
+        )
+        self.url = reverse("email_confirmation")
+        self.verification_code = self.client.session["pending_email_confirmation"][
+            "code"
+        ]
+
+    def test_email_verification_template_render(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Please check your inbox!")
+        self.assertContains(response, "watermelon@farm.com")
+
+    def test_email_verification_success(self):
+        response = self.client.post(
+            self.url,
+            data={"code": self.verification_code},
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        user = User.objects.get(username="watermelon")
+        self.assertTrue(user.profile.email_confirmed)
+        self.assertNotIn("pending_email_confirmation", self.client.session)
--- kara/accounts/urls.py
+++ kara/accounts/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
+    path(
+        "email/confirmation/",
+        views.EmailConfirmationView.as_view(),
+        name="email_confirmation",
+    ),
 ]
