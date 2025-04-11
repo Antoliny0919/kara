f5d39e6308c619c48f51e543b993ac3263867425
antoliny0919 <antoliny0919@gmail.com>
2025-04-11 10:59:28 +0900

    Updated -- UserProfile 프로필 이미지 기본 이미지 추가, 미디어 설정 추가.

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path(settings.ADMIN_URL, admin.site.urls),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+]
+
+urlpatterns += [
     path("", include("kara.home.urls")),
-    path("admin/", admin.site.urls),
     path("accounts/", include("kara.accounts.urls")),
 ]
