f512f27449dd9da14bfd4b13dbc6d90bd1b35357
Antoliny Lee <antoliny0919@gmail.com>
2025-04-11 11:06:31 +0900

    Updated -- UserProfile 프로필 이미지 기본 이미지 추가, 미디어 설정 추가. (#64)

--- kara/accounts/models.py
+++ kara/accounts/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.files.storage import default_storage
 from django.db import models
 
 from .fields import DefaultOneToOneField
@@ -14,7 +15,9 @@ class UserProfile(models.Model):
         User, create=True, on_delete=models.CASCADE, related_name="profile"
     )
     bio = models.TextField(blank=True, null=True)
-    bio_image = models.ImageField(blank=True, null=True)
+    bio_image = models.ImageField(
+        default=default_storage.url("profile/profile_default_image.png")
+    )
     email_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
