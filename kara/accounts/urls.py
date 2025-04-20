98cd356a606aeed8991614b0a6e24207ce35ab16
Antoliny Lee <antoliny0919@gmail.com>
2025-04-21 08:31:27 +0900

    Updated -- 로그아웃 로직 추가. (#79)

--- kara/accounts/urls.py
+++ kara/accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -5,6 +6,7 @@ from . import views
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("signup/", views.SignupView.as_view(), name="signup"),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
     path("profile/", views.ProfileView.as_view(), name="profile"),
     path(
         "email/confirmation/",
