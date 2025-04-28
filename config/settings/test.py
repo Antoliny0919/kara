e28f8536973b0576b572c6c975eb0a80f96be979
antoliny0919 <antoliny0919@gmail.com>
2025-04-29 08:42:32 +0900

    Updated -- ProfileView가 UpdateView를 상속받도록 수정.

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,4 +1,5 @@
 import dj_database_url
+from django.core.files.storage import FileSystemStorage
 
 from .base import *
 
@@ -15,5 +16,38 @@ DATABASES["default"] = dj_database_url.config(
     conn_max_age=600,
     conn_health_checks=True,
 )
+
+# MEDIA
+# ------------------------------------------------------------------------------
+MEDIA_URL = "test_media/"
+
+MEDIA_ROOT = "/test_media"
+
+
+# STORAGE
+# ------------------------------------------------------------------------------
+class CustomTestFileSystemStorage(FileSystemStorage):
+    """
+    In the test environment, the storage overwrites the file
+    and does not use a hash in the file name.
+    """
+
+    def get_available_name(self, name, max_length=None):
+        return name
+
+    def save(self, name, content, max_length=None):
+        if self.exists(name):
+            self.delete(name)
+        return super().save(name, content, max_length)
+
+
+STORAGES = {
+    "default": {
+        "BACKEND": "config.settings.test.CustomTestFileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 LOGGING = {}
