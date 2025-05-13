791e69538595fccd92d6ae21ebd793712b5cbd57
Antoliny Lee <antoliny0919@gmail.com>
2025-05-13 16:51:16 +0900

    Updated -- 축의금 기록 저장소 추가 페이지 업데이트. (#106)

--- kara/base/widgets.py
+++ kara/base/widgets.py
@@ -1,7 +1,10 @@
+from datetime import date
+
 from django.forms import TextInput
 from django.forms.widgets import (
     CheckboxInput,
     EmailInput,
+    MultiWidget,
     PasswordInput,
     RadioSelect,
     Textarea,
@@ -31,3 +34,28 @@ class KaraTextInput(TextInput):
 
 class KaraCheckboxInput(CheckboxInput):
     template_name = "base/widgets/checkbox.html"
+
+
+class KaraSplitDateInput(MultiWidget):
+    template_name = "base/widgets/date.html"
+
+    def __init__(self, attrs=None):
+        widgets = [
+            TextInput(attrs={"placeholder": "YYYY", "maxlength": "4", **(attrs or {})}),
+            TextInput(attrs={"placeholder": "MM", "maxlength": "2", **(attrs or {})}),
+            TextInput(attrs={"placeholder": "DD", "maxlength": "2", **(attrs or {})}),
+        ]
+        super().__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if isinstance(value, date):
+            return [value.day, value.month, value.year]
+        elif isinstance(value, str):
+            year, month, day = value.split("-")
+            return [year, month, day]
+        return [None, None, None]
+
+    def value_from_datadict(self, data, files, name):
+        year, month, day = super().value_from_datadict(data, files, name)
+        # DateField expects a single string that it can parse into a date.
+        return "{}-{}-{}".format(year, month, day)
--- kara/cash_gifts/views.py
+++ kara/cash_gifts/views.py
@@ -1,5 +1,39 @@
-from django.views.generic import TemplateView
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import CreateView, TemplateView
+from django.views.generic.detail import DetailView
+
+from .forms import CashGiftsRecordRepositoryForm
+from .models import CashGiftsRecordRepository
 
 
 class CashGiftsRecordActionSelectView(TemplateView):
     template_name = "cash_gifts/gifts_record_action_select.html"
+
+
+class AddCashGiftsRecordRepositoryView(LoginRequiredMixin, CreateView):
+    template_name = "cash_gifts/gifts_record_repository_add.html"
+    form_class = CashGiftsRecordRepositoryForm
+
+    def form_valid(self, form):
+        form.instance.owner_id = self.request.user.pk
+        self.object = form.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        messages.add_message(
+            self.request,
+            messages.INFO,
+            _(
+                "The cash gifts record repository has been added. "
+                "Go ahead and record the gifts you've received!"
+            ),
+        )
+        return reverse("repository", args=(self.object.pk,))
+
+
+class CashGiftsRecordRepositoryView(LoginRequiredMixin, DetailView):
+    template_name = "cash_gifts/gifts_record_repository_detail.html"
+    model = CashGiftsRecordRepository
