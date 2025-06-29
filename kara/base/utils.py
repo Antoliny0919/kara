d0c22d470d177f120934dbd19f03f0e04f03ac38
antoliny0919 <antoliny0919@gmail.com>
2025-06-29 11:03:49 +0900

    Updated -- GiftTag필드 테이블에 추가.

--- kara/base/utils.py
+++ kara/base/utils.py
@@ -3,3 +3,22 @@ import re
 
 def pascal_to_snake(name):
     return re.sub(r"(?<!^)(?=[A-Z])", "_", name).lower()
+
+
+def get_contrast_color(hex_code):
+    """
+    Calculates the luminance from the given hex code and determines
+    the appropriate foreground color (black or white)
+    for optimal contrast against the background.
+    """
+    hex_code = hex_code.lstrip("#")
+    if len(hex_code) == 3:
+        hex_code = "".join([c * 2 for c in hex_code])
+
+    # Converting hex color to rgb
+    # fmt: off
+    r, g, b = [int(hex_code[i:i + 2], 16) for i in (0, 2, 4)]
+    # fmt: on
+    # Determine luminance
+    luminance = (r * 0.299 + g * 0.587 + b * 0.114) / 255
+    return "black" if luminance > 0.5 else "white"
--- kara/wedding_gifts/tests/test_tables.py
+++ kara/wedding_gifts/tests/test_tables.py
@@ -1,13 +1,15 @@
 from datetime import date
 
-from django.test import RequestFactory, TestCase, override_settings
+from django.test import TestCase, override_settings
+from django.urls import reverse
 
-from kara.base.tests.models import Cake
-from kara.wedding_gifts.tables import GiftTable
-
-
-class CakeTable(GiftTable):
-    pass
+from kara.wedding_gifts.factories import (
+    CashGiftFactory,
+    GiftTagFactory,
+    InKindGiftFactory,
+    UserFactory,
+    WeddingGiftRegistryFactory,
+)
 
 
 @override_settings(DATE_FORMAT="F j, Y")
@@ -16,29 +18,47 @@ class GiftTableTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.cake = Cake.objects.create(
-            name="Cheese Basque Cake",
-            kind="cheese",
-            kind_detail=(
-                "Cheese basque cake with italian cheese made by korea's best baker."
-            ),
-            price=100000000,
-            expiration_date=date(2029, 12, 31),
+        cls.user = UserFactory(
+            username="choco", email="choco@taste.com", password="password"
         )
-        cls.factory = RequestFactory()
-        cls.model = Cake
-        cls.queryset = Cake.objects.all()
+        tag1 = GiftTagFactory.create(owner=cls.user, name="Family", hex_color="#527525")
+        tag2 = GiftTagFactory.create(owner=cls.user, name="Friend", hex_color="#9965BA")
+        registry = WeddingGiftRegistryFactory(owner=cls.user)
+        cls.cash_gift = CashGiftFactory(
+            registry=registry, price=100000000, receipt_date=date(2029, 12, 15)
+        )
+        cls.in_kind_gift = InKindGiftFactory(
+            registry=registry,
+            price=10000,
+            receipt_date=date(2030, 7, 7),
+            kind="appliance",
+            kind_detail="very very loooo ooooo ooooo ooooo ooooo ooooo detail.",
+        )
+        cls.cash_gift.tags.set([tag1, tag2])
+        cls.cash_gift.save()
+        cls.in_kind_gift.tags.set([tag1, tag2])
+        cls.in_kind_gift.save()
+        cls.url = reverse("detail_registry", args=(registry.pk,))
 
-    def test_display_field_value(self):
-        request = self.factory.get("/fake-url/")
-        table = CakeTable(request, self.model, self.queryset)
-        choice_value = table.display_for_value(self.cake, "kind")
-        self.assertEqual(choice_value, "Cheese")
-        price_value = table.display_for_value(self.cake, "price")
-        self.assertEqual(price_value, "100,000,000")
-        truncated_str_value = table.display_for_value(self.cake, "kind_detail")
-        self.assertEqual(
-            truncated_str_value, "Cheese basque cake with italian cheese made by …"
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_table_render_field_value(self):
+        cash_gift_response = self.client.get(f"{self.url}?gift_type=cash")
+        in_kind_gift_response = self.client.get(f"{self.url}?gift_type=in_kind")
+        self.assertContains(in_kind_gift_response, "<td>Appliance</td>")
+        self.assertContains(cash_gift_response, "<td>100,000,000</td>")
+        self.assertContains(
+            in_kind_gift_response,
+            "<td>very very loooo ooooo ooooo ooooo ooooo ooooo …</td>",
         )
-        date_value = table.display_for_value(self.cake, "expiration_date")
-        self.assertEqual(str(date_value), "Dec. 31, 2029")
+        self.assertContains(cash_gift_response, "<td>Dec. 15, 2029</td>")
+        self.assertContains(
+            cash_gift_response,
+            (
+                '<td><ul class="tags">\n'
+                '<li style="background-color: #527525; color: white;">Family</li>\n'
+                '<li style="background-color: #9965BA; color: white;">Friend</li>\n'
+                "</ul></td>"
+            ),
+        )
