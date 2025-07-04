7e54521dfcd913836e5b541d038642d0ad8ca8a1
Antoliny Lee <antoliny0919@gmail.com>
2025-07-04 17:12:29 +0900

    Updated -- 축의금 기록 추가 섹션 헤더 추가, 섹션 헤더 컴포넌트로 분리. (#187)

---
+++ kara/wedding_gifts/translation.py
@@ -0,0 +1,14 @@
+from django.utils.translation import gettext_lazy as _
+
+# translation.py is provided for translation purposes.
+# Add any values that need translation and are passed via variables.
+# The added values will be recognized by makemessages command.
+SECTION_HEADER_TRANSLATIONS = [
+    _("Select Registry"),
+    _(
+        "Please select a repository before saving your record. "
+        "Your saved records will be stored in the selected repository."
+    ),
+    _("Add Wedding Gift Record"),
+    _("Record the wedding gifts you received."),
+]
